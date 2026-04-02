ec9500400efd54aa96e9192f9c99061b65fffc42
avdhutk072-collab <avdhutk072@gmail.com>
2026-04-02 11:16:05 +0530

    Add files via upload

---
+++ list of string 2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  2 10:50:49 2026
+
+@author: User
+"""
+
+# Take input from the user
+string =input("enter a string:")
+#Reverse the string using slicing
+reversed_string = string[::-1]
+#Print the resuit
+print("Reversed string:",reversed_string)
