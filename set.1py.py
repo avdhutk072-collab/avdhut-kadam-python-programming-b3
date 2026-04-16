b86e8b03da143da335c1fb02a86c268bc4e9fd2f
avdhutk072-collab <avdhutk072@gmail.com>
2026-04-16 11:05:18 +0530

    Add files via upload

---
+++ set.1py.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 16 10:32:07 2026
+
+@author: User
+"""
+
+student={}
+student["name"]="rahul"
+student["age"]=20
+student["course"]="python"
+print("dictionary after adding elements:")
+print(student)
+student["age"]=21
+print("\n dictionary after updating a value:")
+del student["course"]
+print("\n dictionary after deleting a key-value pair:")
+print(student)
