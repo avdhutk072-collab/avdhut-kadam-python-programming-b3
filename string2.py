6d6e73b87f4f8709bd0656808e3c1894f23db6a8
avdhutk072-collab <avdhutk072@gmail.com>
2026-04-30 11:11:06 +0530

    Add files via upload

---
+++ string2.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 30 05:21:42 2026
+
+@author: User
+"""
+
+import datetime
+# Get current date and time
+now = datetime.datetime.now()
+# Print current date
+print("Current Date:", now.strftime("%Y-%m-%d"))
+# Print current time
+print("Current Time:", now.strftime("%H:%M:%S"))
+
+# Print current weekday
+print("Weekday:", now.strftime("%A"))
